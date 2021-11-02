7d874f5e6f27bb9f2d857df21e12840a3efdb83c
Alisson Alves <alisson.asilva3@gmail.com>
2021-11-02 16:24:53 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developement by <a href="https://github.com/alissonas3" target="_blank" rel="noreferrer">Alisson Alves</a></p>
+                <p className="text-light"><small><strong>Spring React Week</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
